d334b9fe0b661876e3b2709e886fc05bf530b9bf
Anaïs TORRES <anais@altazion.com>
2022-08-24 16:58:49 +0200

    en cours

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Text.Json;
 
 
@@ -25,34 +26,35 @@ namespace ProjetEvents
             string file = @"C:\Users\AnaïsTORRES\source\repos\ProjetEvents\eventdescriptors.json";
             string fichierEvents = File.ReadAllText(file);
 
-            //LINQ
-            /*using StreamReader fichierEvents = File.OpenText(file);
-            {
-                JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(fichierEvents));
-                /Array eventsDetailsList = (JArray)o["Categories"]["Groups"][0]["Events"];
-                Console.Write(eventsDetailsList);
-               return eventsDetailsList;
-            }*/
-
             if (fichierEvents == null)
                 Console.Write("Pas de fichier json");
 
             List<EventContentRoot> eventsRoot = JsonSerializer.Deserialize<List<EventContentRoot>>(fichierEvents);
+
+
             foreach (EventContentRoot e in eventsRoot)
             {
                 foreach (EventContentCategory c in e.Categories)
                 {
-
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine(" " + c.Name);
+
                     foreach (EventGroup g in c.Groups)
                     {
                         Console.ForegroundColor = ConsoleColor.Cyan;
                         Console.WriteLine(" -- " + g.Name);
+
                         foreach (EventDetails d in g.Events)
                         {
                             Console.ForegroundColor = ConsoleColor.Yellow;
                             Console.WriteLine(" ---- " + d.EventName);
+
+
+                            //Création dossier
+                            string path = (c.Name + "/" + g.Name + "/" + d.EventName).Trim().ToLower().Normalize();
+                            Console.WriteLine("path : " + path);
+
+
                         }
                     }
                 }
