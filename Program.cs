2ccc840ce80bcbdc6f35ab37b380b585b63f794d
Anaïs TORRES <anais@altazion.com>
2022-08-24 10:26:04 +0200

    énumération events

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+
+
+
+namespace ProjetEvents
+{
+    public class Program
+    {
+
+        static void Main()
+        {
+            GetEvents();
+        }
+
+        /// <summary>
+        /// Récupère, désérialise, énumère les différents events 
+        /// </summary>
+        public static void GetEvents()
+        {
+            string file = @"C:\Users\AnaïsTORRES\source\repos\ProjetEvents\eventdescriptors.json";
+            string fichierEvents = File.ReadAllText(file);
+
+            //LINQ
+            /*using StreamReader fichierEvents = File.OpenText(file);
+            {
+                JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(fichierEvents));
+                /Array eventsDetailsList = (JArray)o["Categories"]["Groups"][0]["Events"];
+                Console.Write(eventsDetailsList);
+               return eventsDetailsList;
+            }*/
+
+            if (fichierEvents == null)
+                Console.Write("Pas de fichier json");
+
+            List<EventContentRoot> eventsRoot = JsonSerializer.Deserialize<List<EventContentRoot>>(fichierEvents);
+            foreach (EventContentRoot e in eventsRoot)
+            {
+                foreach (EventContentCategory c in e.Categories)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(" " + c.Name);
+                    foreach (EventGroup g in c.Groups)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine(" -- " + g.Name);
+                        foreach (EventDetails d in g.Events)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine(" ---- " + d.EventName);
+                        }
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+
+}
---
+++ evenements.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetEvents
+{
+    public class EventContentRoot
+    {
+        public EventContentRoot()
+        {
+            Categories = new List<EventContentCategory>();
+        }
+        public List<EventContentCategory> Categories { get; set; }
+    }
+
+    public class EventContentCategory
+    {
+        public EventContentCategory()
+        {
+            Groups = new List<EventGroup>();
+        }
+        public string Name { get; set; }
+        public List<EventGroup> Groups { get; set; }
+    }
+
+    public class EventGroup
+    {
+        public EventGroup()
+        {
+            Events = new List<EventDetails>();
+        }
+        public string Name { get; set; }
+        public List<EventDetails> Events { get; set; }
+    }
+
+    public class EventDetails
+    {
+        public EventDetails()
+        {
+            Types = new List<AideContentDataObject>();
+        }
+
+        public string EventName { get; set; }
+        public string ClassName { get; set; }
+        public string Description { get; set; }
+
+
+        public string CategoryCode { get; set; }
+        public string Code { get; set; }
+
+
+        public List<AideContentDataObject> Types { get; set; }
+    }
+
+    public class AideContentDataObject
+    {
+        public string ClassName { get; set; }
+        public string Description { get; set; }
+        public string Declaration { get; set; }
+        public AideContentDataObjectDetails Details { get; set; }
+    }
+
+    public enum AideContentDataObjectType
+    {
+        Enum,
+        Class
+    }
+
+    public class AideContentDataObjectDetails
+    {
+        public AideContentDataObjectDetails()
+        {
+            Members = new List<AideContentDataObjectMember>();
+        }
+        public AideContentDataObjectType DataType { get; set; }
+        public List<AideContentDataObjectMember> Members { get; set; }
+    }
+
+    public class AideContentDataObjectMember
+    {
+        public string Name { get; set; }
+        public long? EnumValue { get; set; }
+        public string FullTypeName { get; set; }
+    }
+
+
+
+}
